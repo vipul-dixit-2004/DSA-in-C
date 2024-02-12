eb9f367129a61f1aac88730d1d01c87d5c94e573
Vipul Dixit <dixit.vipul0511@gmail.com>
2024-02-12 23:06:04 +0530

    Updated DoubleLinkedList.c

--- LinkedList/DoubleLinkedList.c
+++ LinkedList/DoubleLinkedList.c
@@ -1,6 +1,12 @@
+/*
+ created By Vipul Dixit
+ github link:
+ https://github.com/vipul-dixit-2004/DSA-in-C/blob/main/LinkedList/DoubleLinkedList.c
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-
+void append(int value);
 struct DoubleLinkedList
 {
     int value;
@@ -15,16 +21,6 @@ node *createNode()
 {
     return (node *)malloc(sizeof(node));
 }
-// menu
-int menu()
-{
-    int ch;
-    printf("\n-----------------------------\n1.CreateList\n2.Append\n3.AddAfter\n4.DeleteNode\n5.DeleteElement\n6.Display LinkedList\n7.Exit\n");
-    printf("Enter your Choice: ");
-    scanf("%d", &ch);
-    return ch;
-}
-
 // will append data at end of list
 void append(int value)
 {
@@ -36,7 +32,6 @@ void append(int value)
         newNode->value = value;
         newNode->prev = NULL;
         newNode->next = NULL;
-        return;
     }
     else
     {
@@ -47,7 +42,30 @@ void append(int value)
         newNode->next = NULL;
         tail = newNode;
     }
+    return;
 }
+// menu
+void createList()
+{
+    if (head == NULL)
+    {
+        int data;
+        printf("Enter Value for node: ");
+        scanf("%d", &data);
+        append(data);
+        return;
+    }
+    printf("List Already Exist!! ");
+}
+int menu()
+{
+    int ch;
+    printf("\n-----------------------------\n1.CreateList(Works only in turboC because getchar)\n2.Append\n3.Add at beginning\n4.AddAfter\n5.DeleteNode\n6.DeleteElement\n7.Display LinkedList\n8.Display list from back\n9.Exit\n");
+    printf("Enter your Choice: ");
+    scanf("%d", &ch);
+    return ch;
+}
+
 // insert Element at beginning of the list
 void addhead(int value)
 {
@@ -60,6 +78,35 @@ void addhead(int value)
     newNode->value = value;
     return;
 }
+// insert element at any position
+void insertAfter(int pos, int value)
+{
+    node *temp = head;
+    for (int i = 1; i < pos && temp->next != NULL; i++)
+    {
+        temp = temp->next;
+    }
+    if (temp == NULL)
+    {
+        printf("Enter Position exceeds the length of list!!!");
+        return;
+    }
+    node *newNode = createNode();
+    newNode->value = value;
+    newNode->next = temp->next;
+    newNode->prev = temp;
+    (temp->next)->prev = newNode;
+    temp->next = newNode;
+}
+void insertHead(int value)
+{
+    node *newNode = createNode();
+    newNode->value = value;
+    newNode->prev = NULL;
+    newNode->next = head;
+    head = newNode;
+}
+// delete node
 void deleteNode(int pos)
 {
     if (pos == 1)
@@ -78,6 +125,24 @@ void deleteNode(int pos)
     temp->next->prev = temp->prev;
     free(temp);
 }
+// delete Node by value
+void deleteElement(int value)
+{
+    node *temp = head;
+    while (temp->value != value)
+    {
+        if (temp->next == NULL)
+        {
+            printf("No such element in the list");
+            return;
+        }
+        temp = temp->next;
+    }
+    temp->prev->next = temp->next;
+    temp->next->prev = temp->prev;
+    free(temp);
+}
+
 // to print all the elements of LinkedList;
 void display()
 {
@@ -103,14 +168,52 @@ void displayFromBack()
 }
 int main()
 {
-    append(20);
-    append(30);
-    append(40);
-    append(50);
-    addhead(1000);
-    append(60);
-    display();
-    deleteNode(1);
-    display();
-    // displayFromBack();
+    int value, pos;
+    while (1)
+    {
+        int ch = menu();
+        switch (ch)
+        {
+        case 1:
+            createList();
+            break;
+
+        case 2:
+            printf("Enter Node value to append: ");
+            scanf("%d", &value);
+            append(value);
+            break;
+        case 3:
+            printf("Enter Node value to add at beginning: ");
+            scanf("%d", &value);
+            insertHead(value);
+            break;
+        case 4:
+            printf("Enter Node location to append after (position value): ");
+            scanf("%d %d", &pos, &value);
+            insertAfter(pos, value);
+            break;
+        case 5:
+            printf("Enter the Position you want to delete: \n");
+            scanf("%d", &pos);
+            deleteNode(pos);
+            break;
+        case 6:
+            printf("Enter the value you want to delete: \n");
+            scanf("%d", &value);
+            deleteElement(value);
+            break;
+        case 7:
+            printf("\n-----------------------------\n\n");
+            display();
+            break;
+        case 8:
+            printf("\n-----------------------------\n\n");
+            displayFromBack();
+            break;
+        case 9:
+            return 0;
+        }
+        getchar();
+    }
 }
