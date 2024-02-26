a2dab23421271f5cbe0670b7b7eec4c3b3d7c929
Vipul Dixit <dixit.vipul0511@gmail.com>
2024-02-26 14:15:27 +0530

    stack using linkedlist

--- Stacks/LinkedListStack.c
+++ Stacks/LinkedListStack.c
@@ -27,11 +27,33 @@ void push(int value)
     newNode->next = top;
     top = newNode;
 }
+int peak()
+{
+    if (top == NULL)
+    {
+        return -1;
+    }
+
+    return top->value;
+}
+int menu()
+{
+    printf("\n1.) Push\n2.) Pop\n3.) Peak\n4.) Display\n5.) Exit\n Enter your choice: ");
+    int choice;
+    scanf("%d", &choice);
+    return choice;
+}
+
 int pop()
 {
-    int item = top->value;
-    top = top->next;
-    return item;
+    if (top != NULL)
+    {
+        int item = top->value;
+        top = top->next;
+        return item;
+    }
+    printf("\n The stack is underflow....\n");
+    return -1;
 }
 void display()
 {
@@ -39,11 +61,51 @@ void display()
     temp = top;
     while (temp != NULL)
     {
-        printf("%d-> ", temp->value);
+        printf("%d\n-----\n", temp->value);
         temp = temp->next;
     }
-    printf("NULL\n");
 }
 void main()
 {
+    int bool;
+    int value;
+    while (1)
+    {
+        int ch = menu();
+        switch (ch)
+        {
+
+        case 1:
+            printf("Enter value to push: ");
+            scanf("%d", &value);
+            push(value);
+            break;
+        case 2:
+            bool = pop();
+            if (bool == -1)
+            {
+                printf("The stack is underflow....");
+                break;
+            }
+            printf("The Value at top is: %d", bool);
+            break;
+        case 3:
+            bool = peak();
+            if (bool == -1)
+            {
+                printf("The Stack is empty");
+                break;
+            }
+            printf("The Elemednt at thd top is: %d", bool);
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            return;
+        default:
+            printf("\n______________________________________________\n");
+        }
+        getchar();
+    }
 }
